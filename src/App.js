e4a209a6f2060df50e6997ca603765a8b12e0a31
Mayank697 <smayank2000@gmail.com>
2021-12-21 18:44:29 +0530

    routes-added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,43 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-    <Navbar title="TextUtils" about="About" />
-    <div className="container my-5">
-      {/* <TextForm heading="Enter the text to Analyze"/> */}
-      <About/>
-    </div>
+    <Router>
+      <Navbar title="TextUtils" about="About" mode={mode} toggleMode={toggleMode} />
+      <div className="container my-5">
+        <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+          <Route exact path="/">
+            <TextForm heading="Enter the text to Analyze" mode={mode} />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -23,16 +23,16 @@ export default function TextForm(props) {
 
     return (
         <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handelOnChange} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handelOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color: props.mode === 'dark'?'white':'black' }} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-1" onClick={handelUpperCase}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-1" onClick={handelLowerCase}>Convert to Lowercase</button>
             <button className="btn btn-primary" onClick={clearText}>Clear Text</button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
             <h1>Your Text Summary </h1>
             <p> {text.trim().split(' ').length} words and {text.length} characters </p>
             <p> {0.008 * text.trim().split(' ').length} Minutes read</p>
